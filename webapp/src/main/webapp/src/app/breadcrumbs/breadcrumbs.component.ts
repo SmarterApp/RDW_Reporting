b08ea9e04cc4f2861e3a555f14e905c73a2ed213
Devin Snyder <dsnyder@fairwaytech.com>
2017-10-26 13:47:20 -0700

    DWR-608 We should set the page title based upon the route

--- webapp/src/main/webapp/src/app/breadcrumbs/breadcrumbs.component.ts
+++ webapp/src/main/webapp/src/app/breadcrumbs/breadcrumbs.component.ts
@@ -1,8 +1,10 @@
 import {Component, OnInit} from "@angular/core";
+import { Title } from "@angular/platform-browser";
 import { Router, ActivatedRoute, NavigationEnd, PRIMARY_OUTLET, UrlSegment } from "@angular/router";
 import "rxjs/add/operator/filter";
 import {Utils} from "../shared/Utils";
 import * as _ from "lodash";
+import { TranslateService } from "@ngx-translate/core";
 
 @Component({
   selector: 'breadcrumbs',
@@ -11,13 +13,18 @@ import * as _ from "lodash";
 export class BreadcrumbsComponent implements OnInit{
   breadcrumbs : Array<any> = [];
 
-  constructor(private router: Router, private activatedRoute : ActivatedRoute ) {
+  constructor(private router: Router, private activatedRoute : ActivatedRoute, private title: Title, private translateService: TranslateService ) {
   }
 
   ngOnInit(): void {
     this.router.events.filter(event => event instanceof NavigationEnd).subscribe(() => {
       let root: ActivatedRoute = this.activatedRoute.root;
+      let translatedBreadCrumbs = "";
       this.breadcrumbs = this.getBreadcrumbs(root);
+      for (let breadcrumb of this.breadcrumbs) {
+        translatedBreadCrumbs = translatedBreadCrumbs.concat(this.translateService.instant(breadcrumb.label, breadcrumb.translateParams), " > ");
+      }
+      this.title.setTitle(translatedBreadCrumbs + "Smarter Balanced | Reporting System");
     });
   }
 
