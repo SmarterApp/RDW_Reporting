26cfb84c7c05cc4ac4ca2a5bc7c7b7defc5e62ac
Devin Snyder <dsnyder@fairwaytech.com>
2017-11-08 18:08:02 -0800

    renamed some variables, corrected sql for unrelated query, and angular changes

--- webapp/src/main/webapp/src/app/school-grade/results/school-assessment.service.ts
+++ webapp/src/main/webapp/src/app/school-grade/results/school-assessment.service.ts
@@ -56,12 +56,25 @@ export class SchoolAssessmentService implements AssessmentProvider {
       });
   }
 
-  getAssessmentItems(assessmentId: number) {
-    return this.dataService.get(`/schools/${this.schoolId}/assessmentGrades/${this.grade.id}/assessments/${assessmentId}/examitems`, { search: this.getSchoolYearParams(this.schoolYear) })
-      .catch(ResponseUtils.badResponseToNull)
-      .map(x => {
-        return this.mapper.mapAssessmentItemsFromApi(x);
-      });
+  getAssessmentItems(assessmentId: number, multipleChoiceMultipleSelectItems?: boolean) {
+    if (multipleChoiceMultipleSelectItems) {
+      return this.dataService.get(`/schools/${this.schoolId}/assessmentGrades/${this.grade.id}/assessments/${assessmentId}/examitems`, {
+        params: {
+          types: [ 'MC', 'MS' ],
+          schoolYear: this.schoolYear.toString()
+        }
+      })
+        .catch(ResponseUtils.badResponseToNull)
+        .map(x => {
+          return this.mapper.mapAssessmentItemsFromApi(x);
+        });
+    } else {
+      return this.dataService.get(`/schools/${this.schoolId}/assessmentGrades/${this.grade.id}/assessments/${assessmentId}/examitems`, { search: this.getSchoolYearParams(this.schoolYear) })
+        .catch(ResponseUtils.badResponseToNull)
+        .map(x => {
+          return this.mapper.mapAssessmentItemsFromApi(x);
+        });
+    }
   }
 
   exportItemsToCsv(exportRequest: ExportRequest) {
