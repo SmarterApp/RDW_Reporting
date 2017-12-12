3840391840eb6e7ab80388249e2c6c51d8a09105
Devin Snyder <dsnyder@fairwaytech.com>
2017-12-11 17:43:01 -0800

    extracted method (linkText)

--- webapp/src/main/webapp/src/app/assessments/popover/instructional-resource-popover.component.ts
+++ webapp/src/main/webapp/src/app/assessments/popover/instructional-resource-popover.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { InstructionalResource } from "../model/instructional-resources.model";
+import { TranslateService } from "@ngx-translate/core";
 
 @Component({
   selector: 'instructional-resource-popover',
@@ -7,10 +8,23 @@ import { InstructionalResource } from "../model/instructional-resources.model";
 })
 export class InstructionalResourcePopoverComponent {
 
+  constructor(private translateService: TranslateService) {}
+
   @Input()
   resources: InstructionalResource[];
 
   @Input()
   performanceLevel: number;
+
+  linkText(index: number): string {
+    let resource = this.resources[index];
+    if (resource.organizationLevel === 'System') {
+      return this.translateService.instant('labels.instructional-resources.link.System');
+    }
+    if (resource.organizationLevel === 'State') {
+      return this.translateService.instant('labels.states.' + resource.stateCode) + this.translateService.instant('labels.instructional-resources.link.State');
+    }
+    return this.translateService.instant('labels.instructional-resources.link.' + resource.organizationName, resource);
+  }
 
 }
