e55215244993ef37296f4abdd54bf8dc5c665d03
Jeff Treuting <jtreuting@fairwaytech.com>
2018-01-31 22:42:54 -0800

    formatting

--- webapp/src/main/webapp/src/app/assessments/items/item-scores/student-score.model.ts
+++ webapp/src/main/webapp/src/app/assessments/items/item-scores/student-score.model.ts
@@ -1,6 +1,6 @@
 import { Student } from "../../../student/model/student.model";
 import { School } from "../../../user/model/school.model";
-import {WritingTraitScores} from "../../model/writing-trait-scores.model";
+import { WritingTraitScores } from "../../model/writing-trait-scores.model";
 
 export class StudentScore {
   student: Student;
