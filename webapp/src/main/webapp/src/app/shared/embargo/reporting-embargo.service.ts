cab39dc44ac94423fc2ed42028843982d7a89d5a
wtritch <wtritchler@fairwaytech.com>
2018-02-27 16:01:57 -0800

    Disable CSV Export buttons when user has embargo_read permissions and organizations are embargoed.

--- webapp/src/main/webapp/src/app/shared/embargo/reporting-embargo.service.ts
+++ webapp/src/main/webapp/src/app/shared/embargo/reporting-embargo.service.ts
@@ -2,13 +2,16 @@ import { Injectable } from "@angular/core";
 import { CachingDataService } from "../data/caching-data.service";
 import { Observable } from "rxjs/Observable";
 import { of } from "rxjs/observable/of";
-import { catchError } from "rxjs/operators";
+import { catchError, map } from "rxjs/operators";
 import { ReportingServiceRoute } from "../service-route";
+import { UserService } from "../../user/user.service";
+import { User } from "../../user/model/user.model";
 
 @Injectable()
 export class ReportingEmbargoService {
 
-  constructor(private dataService: CachingDataService) {
+  constructor(private dataService: CachingDataService,
+              private userService: UserService) {
   }
 
   /**
@@ -17,9 +20,15 @@ export class ReportingEmbargoService {
    * @returns {Observable<boolean>}
    */
   isEmbargoed(): Observable<boolean> {
-    return this.dataService.get(`${ReportingServiceRoute}/user-organizations/embargoed`)
+    const embargoEnabled: Observable<boolean> = this.dataService.get(`${ReportingServiceRoute}/user-organizations/embargoed`)
     // fill for backend - if the backend is not there return true by default
       .pipe(catchError(response => of({enabled: true})));
+
+    const embargoRead: Observable<boolean> = this.userService.getCurrentUser()
+      .pipe(map((user: User) => user.permissions.indexOf("EMBARGO_READ") >= 0));
+
+    return Observable.forkJoin(embargoEnabled, embargoRead)
+      .pipe(map(values => values[0] && values[1]));
   }
 
 }
