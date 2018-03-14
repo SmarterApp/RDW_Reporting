ffa5300110ff19af72ef006640ad3dc1b776bc1d
wtritch <wtritchler@fairwaytech.com>
2018-03-14 15:28:50 -0700

    Migrate the distractor analysis table to the new table implementation.

--- webapp/src/main/webapp/src/app/shared/datatable/table-row-expander.component.ts
+++ webapp/src/main/webapp/src/app/shared/datatable/table-row-expander.component.ts
@@ -1,20 +1,20 @@
 import { Component, Input } from "@angular/core";
-import { DataTable } from "primeng/components/datatable/datatable";
+import { Table } from "primeng/table";
 import { Utils } from "../support/support";
 
 /**
- * @deprecated Remove once we no longer have DataTable implementations
+ * This component is responsible for toggling the expansion of a table row.
  */
 @Component({
-  selector: 'datatable-row-expander',
+  selector: 'table-row-expander',
   template: `
     <button class="btn btn-info btn-xs btn-block text-left label-max-width" (click)="toggle()" title="{{text}}"><i class="fa" [ngClass]="{'fa-caret-square-o-down': !expanded, 'fa-caret-square-o-up': expanded, 'mr-xs': hasText}"></i> {{text}}</button>
   `
 })
-export class DataTableRowExpander {
+export class TableRowExpander {
 
   @Input()
-  datatable: DataTable;
+  table: Table;
 
   @Input()
   row: any;
@@ -23,11 +23,11 @@ export class DataTableRowExpander {
   text: string = '';
 
   toggle(): void {
-    this.datatable.toggleRow(this.row);
+    this.table.toggleRow(this.row);
   }
 
   get expanded(): boolean {
-    return this.datatable.isRowExpanded(this.row);
+    return this.table.isRowExpanded(this.row);
   }
 
   get hasText(): boolean {
