9d8e92db858a861f053e08cf93f096856e86c88c
Devin Snyder <dsnyder@fairwaytech.com>
2018-06-04 18:42:14 -0700

    some improvements

--- webapp/src/main/webapp/src/app/aggregate-report/query-forms/claim-report-form.component.ts
+++ webapp/src/main/webapp/src/app/aggregate-report/query-forms/claim-report-form.component.ts
@@ -38,8 +38,6 @@ export class ClaimReportFormComponent extends MultiOrganizationQueryFormComponen
    */
   showAdvancedFilters = false;
 
-  collapsed = true;
-
   claimsBySubject = {};
   selectionBySubject = {};
 
