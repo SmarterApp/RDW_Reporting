10184aa67d571fadd5d92458bb3ae40f2b8cbdf8
wtritch <wtritchler@fairwaytech.com>
2018-06-05 16:12:57 -0700

    Fix ordering to be equal when two rows both have dimension Overall.

--- webapp/src/main/webapp/src/app/aggregate-report/results/aggregate-report-table.component.ts
+++ webapp/src/main/webapp/src/app/aggregate-report/results/aggregate-report-table.component.ts
@@ -30,11 +30,16 @@ const AssessmentLabelOrdering: Ordering<AggregateReportItem> = ordering(byString
   .on(item => item.assessmentLabel);
 
 const ScorableClaimOrdering: Ordering<AggregateReportItem> = ordering(byString)
-  .on(item => item.claimCode);
+  .on(item => {
+    console.log("getting scorable claim code", item.claimCode);
+    return item.claimCode;
+  });
 
 const OrganizationalClaimOrderingProvider: (subjectCode: string) => Ordering<AggregateReportItem> = (subjectCode) =>
   (SubjectClaimOrderings.get(subjectCode) || ordering(byString))
-    .on(item => item.claimCode);
+    .on(item => {
+      return item.claimCode
+    });
 
 const TargetOrdering: Ordering<AggregateReportItem> = ordering(byNumericString)
   .on(item => item.targetCode || item.targetNaturalId);
@@ -198,7 +203,7 @@ export class AggregateReportTableComponent {
    * @param event {{order: number, field: string}} An optional sort event
    */
   public sort(event?: SortEvent): void {
-    const ordering: Comparator<AggregateReportItem>[] = this.getIdentityColumns();
+    const ordering: Comparator<AggregateReportItem>[] = this.getIdentityColumnsComparator();
 
     if (!event.field) {
       // We're not sorting on a field.  Just apply the default column ordering
@@ -423,7 +428,7 @@ export class AggregateReportTableComponent {
    *
    * @returns {Comparator<AggregateReportItem>[]} The ordered list of comparators
    */
-  private getIdentityColumns(): Comparator<AggregateReportItem>[] {
+  private getIdentityColumnsComparator(): Comparator<AggregateReportItem>[] {
     return this.columns
       .map((column: Column) => {
         const ordering = this._orderingByColumnField[ column.field ];
