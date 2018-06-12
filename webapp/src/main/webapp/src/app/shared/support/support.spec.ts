8088ebd6b46381ff693f667db30bfbe9418f39a5
esotrope <llunsford@fairwaytech.com>
2018-06-11 19:09:23 -0700

    fixes scroll to top when page filters are changed

--- webapp/src/main/webapp/src/app/shared/support/support.spec.ts
+++ webapp/src/main/webapp/src/app/shared/support/support.spec.ts
@@ -1,4 +1,4 @@
-import { Utils } from './support';
+import { stripRouteParameters, Utils } from './support';
 
 describe('Utils', () => {
 
@@ -25,6 +25,42 @@ describe('Utils', () => {
   it('should create correct filename appendOrIncrementFileNameSuffix', () => {
     expect(Utils.appendOrIncrementFileNameSuffix('an aggregateReport')).toEqual('an aggregateReport (1)');
     expect(Utils.appendOrIncrementFileNameSuffix('an aggregateReport (1)')).toEqual('an aggregateReport (2)');
+  });
+
+});
+
+describe('stripRouteParameters', () => {
+
+  it('should return null if given null', () => {
+    expect(stripRouteParameters(null)).toBe(null);
+  });
+
+  it('should return empty if the url if empty', () => {
+    expect(stripRouteParameters('')).toBe('');
+  });
+
+  it('should return input when there are no tailing params', () => {
+    expect(stripRouteParameters('/')).toBe('/');
+  });
+
+  it('should return input when there are no tailing params', () => {
+    expect(stripRouteParameters('/fragment')).toBe('/fragment');
+  });
+
+  it('should return input when there is child path without tailing params', () => {
+    expect(stripRouteParameters('/fragment/fragments')).toBe('/fragment/fragments');
+  });
+
+  it('should strip tailing params', () => {
+    expect(stripRouteParameters('/fragment/fragments;param=1')).toBe('/fragment/fragments');
+  });
+
+  it('should strip only tailing params', () => {
+    expect(stripRouteParameters('/a;param1=1/fragments;param2=2')).toBe('/a;param1=1/fragments');
+  });
+
+  it('should preserve query string', () => {
+    expect(stripRouteParameters('/a;param1=1/fragments;param2=2?query=something')).toBe('/a;param1=1/fragments?query=something');
   });
 
 });
