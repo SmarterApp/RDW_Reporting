782ddc49faeb184dc44a1f59673223f39384d171
Devin Snyder <dsnyder@fairwaytech.com>
2018-06-18 15:41:44 -0700

    added schoolYear guard back in

--- webapp/src/main/webapp/src/app/groups/results/group-assessment.resolve.ts
+++ webapp/src/main/webapp/src/app/groups/results/group-assessment.resolve.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { GroupAssessmentService, Search } from './group-assessment.service';
+import { empty } from 'rxjs/observable/empty';
 import { AssessmentExam } from '../../assessments/model/assessment-exam.model';
 
 @Injectable()
@@ -11,6 +12,11 @@ export class GroupAssessmentResolve implements Resolve<AssessmentExam> {
   }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<AssessmentExam> {
+    // if school year isn't specified, don't call to get the most recent assessment
+    const { schoolYear } = route.params;
+    if (schoolYear == null) {
+      return empty();
+    }
     return this.service.getMostRecentAssessment(<Search>route.params);
   }
 
