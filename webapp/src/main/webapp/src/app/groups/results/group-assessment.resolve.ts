3db2fe10411842d23aa742d5d474f305c2058cff
Devin Snyder <dsnyder@fairwaytech.com>
2018-06-18 15:37:58 -0700

    correct "most recent" selected assessment button after viewing assessments from group iab dashboard

--- webapp/src/main/webapp/src/app/groups/results/group-assessment.resolve.ts
+++ webapp/src/main/webapp/src/app/groups/results/group-assessment.resolve.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { GroupAssessmentService, Search } from './group-assessment.service';
-import { empty } from 'rxjs/observable/empty';
 import { AssessmentExam } from '../../assessments/model/assessment-exam.model';
 
 @Injectable()
@@ -12,11 +11,6 @@ export class GroupAssessmentResolve implements Resolve<AssessmentExam> {
   }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<AssessmentExam> {
-    // if there are pre-selected assessments, don't call to get the most recent assessment
-    const { schoolYear, assessmentIds } = route.params;
-    if (schoolYear == null || assessmentIds != null) {
-      return empty();
-    }
     return this.service.getMostRecentAssessment(<Search>route.params);
   }
 
