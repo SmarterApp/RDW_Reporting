9bbb4cec02d84c35e09a4c2aa82caf2741d9b3e7
esotrope <llunsford@fairwaytech.com>
2019-04-16 14:36:52 -0700

    updates aggregate form option defaults

--- webapp/src/main/webapp/src/app/aggregate-report/aggregate-report-options.mapper.ts
+++ webapp/src/main/webapp/src/app/aggregate-report/aggregate-report-options.mapper.ts
@@ -210,7 +210,7 @@ export class AggregateReportOptionsMapper {
     return of(<AggregateReportFormSettings>{
       assessmentType: defaultAssessmentType,
       columnOrder: assessmentDefinition.aggregateReportIdentityColumns.concat(),
-      completenesses: [options.completenesses[0]],
+      completenesses: options.completenesses,
       dimensionTypes: [],
       districts: [],
       includeStateResults: true,
@@ -224,9 +224,8 @@ export class AggregateReportOptionsMapper {
         assessmentDefinition.performanceLevelDisplayTypes[0],
       queryType: options.queryTypes[0],
       reportType: options.reportTypes[0],
-      summativeAdministrationConditions: [
-        options.summativeAdministrationConditions[0]
-      ],
+      summativeAdministrationConditions:
+        options.summativeAdministrationConditions,
       schools: [],
       showEmpty: true,
       generalPopulation: {
