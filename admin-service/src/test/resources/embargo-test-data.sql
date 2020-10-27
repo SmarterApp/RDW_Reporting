fe02a1d1ab1fee9c39badf791f7bb5203e15bbab
Greg Charles <greg.charles@accenture.com>
2020-10-26 19:15:00 -0700

    RDW6: revert some test cases for new embargo until next phase

--- admin-service/src/test/resources/embargo-test-data.sql
+++ admin-service/src/test/resources/embargo-test-data.sql
@@ -8,11 +8,12 @@ insert into state_embargo (school_year, individual, aggregate) values
   (2019, true, true),
   (2020, false, false);
 
-insert into district_embargo (district_id, school_year, subject_id, individual, aggregate) values
-  (-10, 2018, 1, 1, 1),
-  (-20, 2018, 1, 2, 0),
-  (-10, 2017, 1, 2, 2),
-  (-10, 2019, 1, 1, 1);
+insert into district_embargo (district_id, school_year, individual, aggregate) values
+  (-10, 2018, true, true),
+  (-20, 2018, false, null),
+  (-10, 2017, false, false),
+  (-10, 2020, true, true);
+
 
 -- get some summative exams in there to test counts
 
@@ -38,36 +39,3 @@ insert into exam (id, type_id, grade_id, student_id, school_id, opportunity, mig
   (-1, 3, 11, -1, -10, 0, null, 0, 0, 0, 0, 2018, -1, 'v1', 2, 1, 'session1', 2000, 20, 1, '2017-11-01 00:00:00.000000', -1, -1, 0),
   (-2, 3, 11, -2, -10, 0, null, 0, 0, 0, 0, 2018, -1, 'v1', 2, 1, 'session2', 2000, 20, 1, '2017-11-01 00:00:00.000000', -1, -1, 0),
   (-3, 3, 11, -1, -20, 0, null, 0, 0, 0, 0, 2018, -2, 'v1', 2, 1, 'session2', 2000, 20, 1, '2017-11-01 00:00:00.000000', -1, -1, 0);
-
-
-insert into exam_count (school_year, district_id, subject_id, count) values
-(2015, -10, 1, 5),
-(2015, -10, 2, 5),
-(2015, -20, 1, 5),
-(2015, -20, 2, 5),
-(2015, -30, 1, 5),
-(2015, -30, 2, 5),
-(2016, -10, 1, 5),
-(2016, -10, 2, 5),
-(2016, -20, 1, 5),
-(2016, -20, 2, 5),
-(2016, -30, 1, 5),
-(2016, -30, 2, 5),
-(2017, -10, 1, 5),
-(2017, -10, 2, 5),
-(2017, -20, 1, 5),
-(2017, -20, 2, 5),
-(2017, -30, 1, 5),
-(2017, -30, 2, 5),
-(2018, -10, 1, 5),
-(2018, -10, 2, 5),
-(2018, -20, 1, 5),
-(2018, -20, 2, 5),
-(2018, -30, 1, 5),
-(2018, -30, 2, 5),
-(2019, -10, 1, 5),
-(2019, -10, 2, 5),
-(2019, -20, 1, 5),
-(2019, -20, 2, 5),
-(2019, -30, 1, 5),
-(2019, -30, 2, 5)
