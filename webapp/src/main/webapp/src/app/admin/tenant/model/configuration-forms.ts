7af44a32870176aa328684b305a3bfc61e0b6647
Greg Charles <greg.charles@accenture.com>
2020-12-04 20:09:55 -0800

    RDW6,req1: (WIP) Expose embargo properties to tenant config

--- webapp/src/main/webapp/src/app/admin/tenant/model/configuration-forms.ts
+++ webapp/src/main/webapp/src/app/admin/tenant/model/configuration-forms.ts
@@ -203,6 +203,26 @@ export function dataSourceFieldConfigurations(
   }));
 }
 
+function embargoFieldConfigurations(): FormFieldConfiguration[] {
+  return [
+    {
+      name: 'embargo.pageSize',
+      dataType: positiveIntegerDataType,
+      required: true
+    },
+    {
+      name: 'embargo.auditLogDays',
+      dataType: positiveIntegerDataType,
+      required: true
+    },
+    {
+      name: 'embargo.auditLogFileName',
+      dataType: stringDataType,
+      required: true
+    }
+  ];
+}
+
 function reportingFieldConfigurations(): FormFieldConfiguration[] {
   return [
     {
@@ -301,6 +321,7 @@ const configurations = [
   ...aggregateFieldConfigurations(),
   ...archiveFieldConfigurations(),
   ...dataSourceFieldConfigurations(),
+  ...embargoFieldConfigurations(),
   ...reportingFieldConfigurations(),
   ...validationFieldConfigurations(),
   ...taskServiceArtClientFieldConfigurations(),
@@ -324,6 +345,7 @@ export function configurationsFormFieldConfigurations(
       ...aggregateFieldConfigurations(),
       ...archiveFieldConfigurations(readonly),
       ...dataSourceFieldConfigurations(readonly),
+      ...embargoFieldConfigurations(),
       ...reportingFieldConfigurations(),
       ...validationFieldConfigurations(),
       ...taskServiceArtClientFieldConfigurations(),
@@ -333,6 +355,7 @@ export function configurationsFormFieldConfigurations(
   } else {
     return [
       ...aggregateFieldConfigurations(),
+      ...embargoFieldConfigurations(),
       ...reportingFieldConfigurations()
     ];
   }
